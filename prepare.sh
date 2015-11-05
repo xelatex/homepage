bb8f8fe1a8b810091f3fa3c059e00fa6b24f22e8
Arthur Chunqi Li <chunqi.li.pku@gmail.com>
2015-11-06 01:27:15 +0800

    Change title and achive rules
    Signed-off-by: Arthur Chunqi Li <chunqi.li.pku@gmail.com>

---
+++ prepare.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+#npm install -g hexo-cli
+#npm install hexo --save
+npm install hexo hexo-generator-index hexo-generator-archive hexo-generator-category hexo-generator-tag hexo-server hexo-deployer-git hexo-deployer-heroku hexo-deployer-rsync hexo-deployer-openshift hexo-renderer-marked@0.2 hexo-renderer-stylus@0.2 hexo-generator-feed@1 hexo-generator-sitemap@1
+
